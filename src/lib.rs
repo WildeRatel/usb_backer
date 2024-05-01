dd3626a46f856d0482a2846cd1516fc8c585111d
Ruben <ruben-f@outlook.com>
2024-05-01 12:36:38 +0200

    0.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-pub fn test_for_drive() {
-    let mut test_for_drive = std::process::Command::new("ls");
-    test_for_drive.output();
+pub fn test_for_drive() -> std::result::Result<Vec<String>, std::io::Error> {
+    let test_for_drive = std::process::Command::new("ls").output()?;
+    let test_output = String::from_utf8(test_for_drive.stdout).unwrap();
+    let test_output_vec: Vec<String> = test_output
+        .split('\n')
+        .map(|x| str::to_string(x).clone())
+        .collect();
+
+    Ok(test_output_vec)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 fn main() {
-    let test_output = String::from_utf8(test_for_drive.stdout).unwrap();
-    let test_output_vec: Vec<String> = test_output
-        .split('\n')
-        .map(|x| str::to_string(x).clone())
-        .collect();
-
     for i in test_output_vec.iter() {
         print!("{}", i);
     }
