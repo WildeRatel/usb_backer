c7012fe8a6b55bae2c18761456f8d8a2ef524a48
Ruben <ruben-f@outlook.com>
2024-05-01 12:40:13 +0200

    0.0.5

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    for i in test_output_vec.iter() {
-        print!("{}", i);
+    if let Err(e) = usb_backer::test_for_drive() {
+        panic!("Couldn't find drive: {e}")
+    } else {
+        let output_vec = usb_backer::test_for_drive().unwrap();
+
+        for i in output_vec.iter() {
+            print!("{}", i);
+        }
     }
 }
