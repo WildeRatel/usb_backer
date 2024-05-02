2bd0376c4015c78bc7e2f64aef4f512e9eaec9a1
Ruben <ruben-f@outlook.com>
2024-05-02 16:43:43 +0200

    0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::os::unix::process::CommandExt;
-
 pub fn load_configs(config_string: String) -> std::result::Result<Vec<String>, std::io::Error> {
     let mut combiner = std::path::PathBuf::new();
     let get_current_dir = std::env::current_exe().unwrap().to_path_buf();
@@ -15,8 +13,14 @@ pub fn load_configs(config_string: String) -> std::result::Result<Vec<String>, s
     Ok(config_lines)
 }
 pub fn copy_over(dir_from: String, dir_to: String) -> std::result::Result<(), std::io::Error> {
-    let _ = std::process::Command::new("cp")
-        .args(["-r", &dir_from, &dir_to])
-        .exec();
+    let mut com = std::process::Command::new("cp");
+    com.current_dir("/");
+    com.arg("-r")
+        .arg(&dir_from)
+        .arg(&dir_to)
+        .output()
+        .expect("Fail!");
+
+    println!("{} {}", dir_from, dir_to);
     Ok(())
 }
