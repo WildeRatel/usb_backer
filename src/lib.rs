b4571bc43a6e3531b0ae02e949e1cde91ba107b4
Ruben <ruben-f@outlook.com>
2024-05-03 15:47:09 +0200

    0.1.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
-pub fn load_configs(config_string: String) -> std::result::Result<Vec<String>, std::io::Error> {
+pub fn load_configs(
+    config_string: String,
+) -> std::result::Result<(Vec<String>, Vec<String>), std::io::Error> {
     let mut combiner = std::path::PathBuf::new();
     let get_current_dir = std::env::current_exe().unwrap().to_path_buf();
     let mut config_lines: Vec<String> = Vec::new();
+    let mut file_transfer: Vec<String> = Vec::new();
 
     combiner.push(get_current_dir);
     combiner.pop();
     combiner.push(std::path::PathBuf::from(config_string));
 
-    for i in std::fs::read_to_string(combiner).unwrap().lines() {
-        config_lines.push(i.to_string());
+    for (x, i) in std::fs::read_to_string(combiner)
+        .unwrap()
+        .lines()
+        .enumerate()
+    {
+        if x <= 1 {
+            config_lines.push(i.to_string());
+        } else {
+            file_transfer.push(i.to_string());
+        }
     }
-    Ok(config_lines)
+    Ok((config_lines, file_transfer))
 }
 pub fn copy_over(dir_from: String, dir_to: String) -> std::result::Result<(), std::io::Error> {
     if cfg!(unix) {
