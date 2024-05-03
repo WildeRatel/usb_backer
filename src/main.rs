cfffd42ddc93b107bc5836d73c5ecbd7ae42d658
Ruben <ruben-f@outlook.com>
2024-05-03 15:56:19 +0200

    0.1.7

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 fn main() {
-    if let Ok(config_lines) = usb_backer::load_configs(String::from("config.txt")) {
-        let mut dir_from = String::new();
-        dir_from.push_str(&config_lines[0]);
-        dir_from.push_str("");
-        dir_from.push_str(&config_lines[1]);
+    if let Ok((config_lines, file_transfer)) = usb_backer::load_configs(String::from("config.txt"))
+    {
+        for (i, x) in file_transfer.iter().enumerate() {
+            println!("{}", x);
 
-        let mut dir_to = String::new();
-        dir_to.push_str(&config_lines[2]);
+            if i == file_transfer.len() - 1 {
+                break;
+            }
 
-        usb_backer::copy_over(dir_from, dir_to).expect("Failed to copy!");
+            let mut dir_from = String::new();
+            dir_from.push_str(&config_lines[0]);
+            dir_from.push_str("");
+            dir_from.push_str(&config_lines[1]);
+
+            let mut dir_to = String::new();
+            dir_to.push_str(&config_lines[i]);
+
+            usb_backer::copy_over(dir_from, dir_to).expect("Failed to copy!");
+        }
     }
 }
